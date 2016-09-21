3294b08775be5c7296124364e65873d23f263fac
Alan Russell <ajrussellaudio@gmail.com>
2016-09-21 19:41:14 +0100

    add first and second verse tests

--- specs/spec.js
+++ specs/spec.js
@@ -2,6 +2,9 @@ var assert = require( "assert" );
 var Hero = require( "../hero" );
 var You = require( "../you" );
 
+var you = new You( "baby" );
+var hero = new Hero( "Enrique" );
+
 var chorus = function() {
   describe( "Hero", function() {
 
@@ -9,8 +12,6 @@ var chorus = function() {
     var hero;
 
     beforeEach(function() {
-      you = new You( "baby" );
-      hero = new Hero( "Enrique" );
     })
 
     it("can be your hero baby", function() {
@@ -34,5 +35,29 @@ var chorus = function() {
 
   })
 };
+
+describe("You", function() {
+
+  it("would dance if I asked you to dance");
+
+  it("would run and never look back");
+
+  it("would cry if you saw me crying");
+
+  it("would save my soul tonight");
+
+});
+
+describe("You", function() {
+
+  it("would tremble if I touched your lips")
+
+  it("would laugh (oh please tell me this)")
+
+  it("would die for the one you love")
+
+  it("can hold me in your arms tonight")
+
+})
 
 chorus();
