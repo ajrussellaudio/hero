f5238d5803df617823e185191af2e01b84138cfa
Alan Russell <ajrussellaudio@gmail.com>
2016-09-21 19:07:00 +0100

    add chorus test

---
+++ specs/spec.js
@@ -0,0 +1,13 @@
+var assert = require( "assert" );
+var Hero = require( "../hero" );
+
+var chorus = function() {
+  describe( "Hero", function() {
+    it("can be your hero baby");
+    it("can kiss away the pain");
+    it("will stand by you forever");
+    it("can have breath taken away by you")
+  })
+}
+
+chorus();
